7f51c4e881269aa6179aaa955040070c0e08e489
Adam Farah <afarah.business@gmail.com>
2019-01-25 19:58:02 -0500

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap bkgnd("/home/student/Desktop/Animal_Shelter_System/havanese_silk_dog.jpg");
+    bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Background, bkgnd);
+    this->setPalette(palette);
 }
 
 MainWindow::~MainWindow()
