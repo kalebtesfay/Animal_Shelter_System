6ddb73b2927d417212eaa79326cb1b53bb912780
kalebtesfay <kalebtesfay@cmail.carleton.ca>
2019-02-02 19:08:37 -0500

    database:rocket:

--- main.cpp
+++ main.cpp
@@ -1,15 +1,3 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * */
-/*                                                 */
-/*  Program:  Simple Event Management System       */
-/*  Author:   Christine Laurendeau                 */
-/*  Date:     07-JUN-2018                          */
-/*                                                 */
-/*  (c) 2018 Christine Laurendeau                  */
-/*  All rights reserved.  Distribution and         */
-/*  reposting, in part or in whole, without the    */
-/*  written consent of the author, is illegal.     */
-/*                                                 */
-/* * * * * * * * * * * * * * * * * * * * * * * * * */
 using namespace std;
 
 #include <iostream>
