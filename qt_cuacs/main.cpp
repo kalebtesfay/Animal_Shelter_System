5d710eee69ad33517e0fc95f2f60830357dd3138
kalebtesfay <kalebtesfay@cmail.carleton.ca>
2019-02-06 21:26:33 -0500

    database connected:rocket:

--- qt_cuacs/main.cpp
+++ qt_cuacs/main.cpp
@@ -2,6 +2,8 @@ using namespace std;
 #include <iostream>
 #include <string>
 #include <QCoreApplication>
+#include <QtSql>
+#include <QDebug>
 #include "Control.h"
 
 int main(int argc, char *argv[])
@@ -9,6 +11,15 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     Control control;
     control.launch();
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("/home/student/COMP3004/Animal_Shelter_System/qt_cuacs/database/database");
+    //db.open();
+
+    if(db.open()){
+        qDebug() << "DB CONNECTED";
+    }else{
+        qDebug() << "DB NOT CONNECTED" << db.lastError();
+    }
 
     return a.exec();
 }
