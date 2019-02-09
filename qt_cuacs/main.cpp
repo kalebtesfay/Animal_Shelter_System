6d2868609bb6467639ba06c9a4425f00df341af9
kalebtesfay <kalebtesfay@cmail.carleton.ca>
2019-02-09 15:09:44 -0500

    database works:rocket:

--- qt_cuacs/main.cpp
+++ qt_cuacs/main.cpp
@@ -3,6 +3,10 @@ using namespace std;
 #include <string>
 #include <QCoreApplication>
 #include <QtSql>
+#include <QSqlDatabase>
+#include <QSqlDriver>
+#include <QSqlError>
+#include <QSqlQuery>
 #include <QDebug>
 #include "Control.h"
 
@@ -20,6 +24,54 @@ int main(int argc, char *argv[])
     }else{
         qDebug() << "DB NOT CONNECTED" << db.lastError();
     }
+    QSqlQuery query("CREATE TABLE ANIMAL(id INTEGER PRIMARY KEY, name TEXT NOT NULL,type TEXT NOT NULL, sex TEXT NOT NULL, age INT, height TEXT NOT NULL, colour TEXT NOT NULL, breed TEXT NOT NULL, neutered TEXT NOT NULL, condition TEXT NOT NULL)");
+    QSqlQuery insertToAnimal;
+    insertToAnimal.exec("insert into ANIMAL(id, name, type, sex, age, height, colour, breed, neutered, condition)"
+                       "values(1, 'Rex', 'Dog', 'Male', 2, '50cm', 'Black', 'Pitbull', 'No', 'Healthy')");
+    insertToAnimal.exec("insert into ANIMAL(id, name, type, sex, age, height, colour, breed, neutered, condition)"
+                       "values(2, 'Daisy', 'Dog', 'Female', 1, '30cm', 'Orange', 'Boxer', 'Yes', 'Healthy')");
+    insertToAnimal.exec("insert into ANIMAL(id, name, type, sex, age, height, colour, breed, neutered, condition)"
+                       "values(3, 'Coco', 'Cat', 'Female', 6, '20cm', 'Brown', 'Persian', 'Yes', 'Not Healthy')");
+    insertToAnimal.exec("insert into ANIMAL(id, name, type, sex, age, height, colour, breed, neutered, condition)"
+                       "values(4, 'Jack', 'Cat', 'Male', 5, '10cm', 'White', 'Ragdoll', 'Yes', 'Healthy')");
+    insertToAnimal.exec("insert into ANIMAL(id, name, type, sex, age, height, colour, breed, neutered, condition)"
+                       "values(5, 'Gus', 'Hamster', 'Male', 1, '6cm', 'Brown', 'Chinese', 'Yes', 'Healthy')");
+
+    QSqlQuery query2("CREATE TABLE STAFF(id INTEGER PRIMARY KEY, name TEXT NOT NULL)");
+    QSqlQuery insertToStaff;
+    insertToStaff.exec("insert into STAFF(id, name)"
+                       "values(1, 'Kaleb Tesfay')");
+    insertToStaff.exec("insert into STAFF(id, name)"
+                       "values(2, 'Adam Farah')");
+    insertToStaff.exec("insert into STAFF(id, name)"
+                       "values(3, 'Japi Sandhu')");
+    insertToStaff.exec("insert into STAFF(id, name)"
+                       "values(4, 'Lauryn Esparza')");
+    insertToStaff.exec("insert into STAFF(id, name)"
+                       "values(5, 'Jaiden Kelly')");
+
+    QSqlQuery query3("CREATE TABLE CLIENT(id INTEGER PRIMARY KEY, name TEXT NOT NULL, address TEXT NOT NULL)");
+    QSqlQuery insertToClient;
+    insertToClient.exec("insert into CLIENT(id, name, address)"
+                        "values(1, 'Wilfred James', '3670 Glory Road')");
+    insertToClient.exec("insert into CLIENT(id, name, address)"
+                        "values(2, 'Mona Lane', '2257 Berry Street')");
+    insertToClient.exec("insert into CLIENT(id, name, address)"
+                        "values(3, 'Steve Blake', '3441 Cemetery Street')");
+    insertToClient.exec("insert into CLIENT(id, name, address)"
+                        "values(4, 'Josh Rich', '2795 Green Hill Road')");
+    insertToClient.exec("insert into CLIENT(id, name, address)"
+                        "values(5, 'Marlee Sparks', '4162 Adams Drive')");
+
+
+//    QSqlQuery q;
+//    q.exec("SELECT * FROM ANIMAL where id > 0");
+
+//    while(q.next()){
+//        //QString name = query.value(0).toString();
+//        int id = query.value(0).toInt();
+//        qDebug() << id;
+//    }
 
     return a.exec();
 }
