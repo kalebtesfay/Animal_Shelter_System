16cbecf6c0ea95a99190324e8233024ac34195f1
kalebtesfay <kalebtesfay@cmail.carleton.ca>
2019-02-09 20:03:50 -0500

    ui folder added:rocket:

--- qt_cuacs/main.cpp
+++ qt_cuacs/main.cpp
@@ -9,14 +9,11 @@ using namespace std;
 #include <QSqlQuery>
 #include <QDebug>
 #include "Control.h"
-#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     Control control;
     control.launch();
-    MainWindow w;
-    w.show();
     return a.exec();
 }
