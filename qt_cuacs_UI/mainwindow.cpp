4c1eb27f0b852ac9998d23dd79934c3faf446451
adam-farah <afarah.business@gmail.com>
2019-02-16 18:04:53 -0500

    Back Button for Staff menu

--- qt_cuacs_UI/mainwindow.cpp
+++ qt_cuacs_UI/mainwindow.cpp
@@ -190,3 +190,12 @@ void MainWindow::on_pushButton_9_clicked()
 {
     ui->stackedWidget->setCurrentIndex(0);
 }
+
+/*
+ * Back Button for
+ *  staff login
+ */
+void MainWindow::on_pushButton_11_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
