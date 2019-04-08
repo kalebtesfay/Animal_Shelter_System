8e95543955e210176993c8d51af52d95d044f7c5
kalebtesfay <kalebtesfay@cmail.carleton.ca>
2019-04-08 16:38:45 -0400

    ACM Contructor uses Animal/Client :rocket:

--- qt_cuacs_UI/ACM.cpp
+++ qt_cuacs_UI/ACM.cpp
@@ -4,9 +4,17 @@ ACM::ACM(QString type, QString breed, QString name, QString sex,
          QString age, QString social, QString condition, QString diet,
          QString train, QString nocturnal, QString emotion,
          QString petWorth, QString parental, QString allergies,
-         QString stability, QString res) : Animal(type, breed, name,
-                                                  sex, age, social, condition, diet, train, nocturnal,
-                                                  emotion, petWorth, parental, allergies, stability, res){}
+         QString stability, QString res,QString id, QString name2, QString address2, QString age2,
+         QString social2, QString condition2, QString diet2, QString train2,
+         QString nocturnal2, QString emotion2, QString petWorth2, QString parental2,
+         QString allergies2, QString stability2, QString res2) : Animal(type, breed, name,
+                                                  sex, age, social, condition,
+                                                  diet, train, nocturnal, emotion,
+                                                  petWorth, parental, allergies, stability,
+                                                  res), Client(id, name2, address2, age2,
+                                                               social2, condition2, diet2, train2,
+                                                               nocturnal2, emotion2, petWorth2, parental2,
+                                                               allergies2, stability2, res2){}
 
 ACM::~ACM(){}
 
--- qt_cuacs_UI/ACM.h
+++ qt_cuacs_UI/ACM.h
@@ -5,17 +5,22 @@
 #include "AnimalList.h"
 
 
-class ACM : public Animal
+class ACM : public Animal, public Client
 {
 public:
     ACM(QString, QString, QString, QString,
         QString, QString, QString, QString,
         QString, QString, QString, QString,
-        QString, QString, QString, QString);
+        QString, QString, QString, QString,
+        QString, QString, QString, QString,
+        QString, QString, QString, QString,
+        QString, QString, QString, QString,
+        QString, QString, QString);
+
     ~ACM();
     int scoringSystem();
 
-private:
+protected:
     //Animal& a;
     int weight;
     int scoring;
@@ -35,6 +40,22 @@ private:
     QString allergies;
     QString stability;
     QString res;
+
+    QString id;
+    QString name2;
+    QString address2;
+    QString age2;
+    QString social2;
+    QString condition2;
+    QString diet2;
+    QString train2;
+    QString nocturnal2;
+    QString emotion2;
+    QString petWorth2;
+    QString parental2;
+    QString allergies2;
+    QString stability2;
+    QString res2;
 };
 
 #endif // ACM_H
