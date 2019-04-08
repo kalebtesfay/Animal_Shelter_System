c10a9a001f521a1c6b31fd56dd43db3661d3b944
kalebtesfay <kalebtesfay@cmail.carleton.ca>
2019-04-08 15:35:25 -0400

    ACM and Animal Objects merged:rocket:

--- qt_cuacs_UI/ACM.cpp
+++ qt_cuacs_UI/ACM.cpp
@@ -1,8 +1,15 @@
 #include "ACM.h"
 
-ACM::ACM()
-{
-
-}
+ACM::ACM(QString type, QString breed, QString name, QString sex,
+         QString age, QString social, QString condition, QString diet,
+         QString train, QString nocturnal, QString emotion,
+         QString petWorth, QString parental, QString allergies,
+         QString stability, QString res) : Animal(type, breed, name,
+                                                  sex, age, social, condition, diet, train, nocturnal,
+                                                  emotion, petWorth, parental, allergies, stability, res){}
 
 ACM::~ACM(){}
+
+int ACM::scoringSystem(){
+
+}
--- qt_cuacs_UI/ACM.h
+++ qt_cuacs_UI/ACM.h
@@ -2,15 +2,39 @@
 #define ACM_H
 #include <QString>
 #include <QDebug>
+#include "AnimalList.h"
 
 
-class ACM
+class ACM : public Animal
 {
 public:
-    ACM();
+    ACM(QString, QString, QString, QString,
+        QString, QString, QString, QString,
+        QString, QString, QString, QString,
+        QString, QString, QString, QString);
     ~ACM();
+    int scoringSystem();
+
 private:
-    QString str;
+    //Animal& a;
+    int weight;
+    int scoring;
+    QString type;
+    QString breed;
+    QString name;
+    QString sex;
+    QString age;
+    QString social;
+    QString condition;
+    QString diet;
+    QString train;
+    QString nocturnal;
+    QString emotion;
+    QString petWorth;
+    QString parental;
+    QString allergies;
+    QString stability;
+    QString res;
 };
 
 #endif // ACM_H
