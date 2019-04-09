6db2a0ed24be2a9ad4268721d2477c0a9c8a799a
adam-farah <afarah.business@gmail.com>
2019-04-09 00:41:26 -0400

    What are doing? :milky_way:

--- qt_cuacs_UI/ACM.cpp
+++ qt_cuacs_UI/ACM.cpp
@@ -25,4 +25,46 @@ ACM::~ACM(){}
 
 int ACM::scoringSystem(){
 
+
+
+ //all clients
+// QSqlQuery client_query;
+// client_query.exec("SELECT id”);
+
+
+////add all client objects to a list from SQL
+//while (client_query.next()) {
+//	//construct object
+//	constructor() {
+//       	 QString name = query.value(0).toString();
+//      	  int salary = query.value(1).toInt();
+//	}
+
+//	list<client> client_object_list;
+
+//	client item;
+//	client_object_list.push_back(item);
+
+//}
+//“same thing for animals ”
+//animal_object_list
+
+//for all client_object_list [ i ] {
+
+//	//filterstage1 for age
+//	for all animal_object_list [ j ] {
+//		if ( client_object_list[i].age == x ) &&
+//	((animal_object_list[ j ].age == x+1) || (animal_object_list[ j ].age == x-1 ) ||  (animal_object_list[ j ].age == x )
+
+//	valid_animals_objects += animal_object_list[ j ]
+
+////filterstage2 for social
+//	for all valid_animals_objects [ j ] {
+//		if ( client_object_list[i].social == x ) &&
+//	((animal_object_list[ j ].social == x+1) || (valid_animals_objects[ j ].social == x-1 ) ||  (valid_animals_objects[ j ].social == x )
+
+//	valid_animals_objects_filter2 += valid_animals_objects[ j ];
+//        }
+
+
 }
